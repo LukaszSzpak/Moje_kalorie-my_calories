7966806b1f8fc4bf0e471c99a19f395acb091619
Łukasz Szpak <alukasz1@interia.pl>
2021-01-15 20:17:41 +0100

    Add text translator

---
+++ myCalories/calories/services/translation.py
@@ -0,0 +1,18 @@
+from google_trans_new import google_translator as Translator
+
+
+def translate_polish_to_english(polish_sentence):
+    return __my_translate(sentence=polish_sentence,
+                          source_lang='pl',
+                          dest_lang='en')
+
+
+def translate_english_to_polish(english_sentence):
+    return __my_translate(sentence=english_sentence,
+                          source_lang='en',
+                          dest_lang='pl')
+
+
+def __my_translate(sentence, source_lang, dest_lang):
+    translator = Translator()
+    return translator.translate(text=sentence, lang_src=source_lang, lang_tgt=dest_lang)
