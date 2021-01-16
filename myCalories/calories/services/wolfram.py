43723389848bab69c387ba815bdde9707ef1d603
Łukasz Szpak <alukasz1@interia.pl>
2021-01-16 14:26:20 +0100

    Add Wolfram api

---
+++ myCalories/calories/services/wolfram.py
@@ -0,0 +1,26 @@
+import requests
+
+from calories.models import Food
+from calories.services.textCoversion import get_number_from_wolfram_response
+
+__APP_ID = '4U9627-RPWXQY9632'
+
+
+def get_food_data_from_wolfram(food_name, unit):
+    food = Food()
+    food.name = food_name
+    food.unit = unit
+    food.calories = __get_one_attribute('calories', food_name, unit)
+    food.fat = __get_one_attribute('fat', food_name, unit)
+    food.protein = __get_one_attribute('protein', food_name, unit)
+    food.carbohydrates = __get_one_attribute('carbohydrates', food_name, unit)
+    return food
+
+
+def __get_one_attribute(attribute, food_name, unit):
+    my_param = '{0}+{1}+{2}'.format(attribute, unit, food_name)
+    url = 'https://api.wolframalpha.com/v1/result?i={0}%3F&appid={1}'.format(my_param, __APP_ID)
+
+    response = requests.get(url)
+    text_response = response.text
+    return get_number_from_wolfram_response(text_response)
