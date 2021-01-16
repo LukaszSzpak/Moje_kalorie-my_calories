a9327712afba0d5a2d26af9a9667f0741017b6b6
Łukasz Szpak <alukasz1@interia.pl>
2021-01-16 14:46:12 +0100

    Add response units

--- myCalories/calories/services/wolfram.py
+++ myCalories/calories/services/wolfram.py
@@ -10,15 +10,15 @@ def get_food_data_from_wolfram(food_name, unit):
     food = Food()
     food.name = food_name
     food.unit = unit
-    food.calories = _get_one_attribute('calories', food_name, unit)
-    food.fat = _get_one_attribute('fat', food_name, unit)
-    food.protein = _get_one_attribute('protein', food_name, unit)
-    food.carbohydrates = _get_one_attribute('carbohydrates', food_name, unit)
+    food.calories = _get_one_attribute('calories', food_name, unit, '')
+    food.fat = _get_one_attribute('fat', food_name, unit, 'in grams')
+    food.protein = _get_one_attribute('protein', food_name, unit, 'in grams')
+    food.carbohydrates = _get_one_attribute('carbohydrates', food_name, unit, 'in grams')
     return food
 
 
-def _get_one_attribute(attribute, food_name, unit):
-    my_param = '{0}+{1}+{2}'.format(attribute, unit, food_name)
+def _get_one_attribute(attribute, food_name, unit, result_unit):
+    my_param = '{0}+{1}+{2} {3}'.format(attribute, unit, food_name, result_unit)
     url = 'https://api.wolframalpha.com/v1/result?i={0}%3F&appid={1}'.format(my_param, __APP_ID)
 
     response = requests.get(url)
