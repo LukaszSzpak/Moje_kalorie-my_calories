eb20080638f92d205470249b9652464033b4e22f
Łukasz Szpak <alukasz1@interia.pl>
2021-01-16 14:13:43 +0100

    Add textConversion functions

--- myCalories/calories/services/textCoversion.py
+++ myCalories/calories/services/textCoversion.py
@@ -1,2 +1,7 @@
 def upper_first_letter(sentence):
     return sentence[0].upper()+sentence[1:]
+
+
+def get_number_from_wolfram_response(text):
+    text_number = text.split()[1]
+    return float(text_number)
