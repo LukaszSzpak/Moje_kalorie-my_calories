1ba846d32317bcb0b4b86957ad2aea70c50e9f6e
Łukasz Szpak <alukasz1@interia.pl>
2021-01-27 14:48:28 +0100

    Fix wolfram whitespaces bug /2

--- myCalories/calories/services/wolfram.py
+++ myCalories/calories/services/wolfram.py
@@ -16,18 +16,16 @@ def get_food_data_from_wolfram(food_name, unit):
     food_name = food_name.replace(" ", "+")
 
     food.calories = _get_one_attribute('calories+in', food_name, unit, '')
-    food.fat = _get_one_attribute('fat+in', food_name, unit, 'in grams')
-    food.protein = _get_one_attribute('protein+in', food_name, unit, 'in grams')
-    food.carbohydrates = _get_one_attribute('carbohydrates+in', food_name, unit, 'in grams')
+    food.fat = _get_one_attribute('fat+in', food_name, unit, 'in+grams')
+    food.protein = _get_one_attribute('protein+in', food_name, unit, 'in+grams')
+    food.carbohydrates = _get_one_attribute('carbohydrates+in', food_name, unit, 'in+grams')
     return food
 
 
 def _get_one_attribute(attribute, food_name, unit, result_unit):
     my_param = '{0}+{1}+{2}+{3}'.format(attribute, unit, food_name, result_unit)
     url = 'https://api.wolframalpha.com/v1/result?i={0}%3F&appid={1}'.format(my_param, __APP_ID)
-    print(url)
 
     response = requests.get(url)
     text_response = response.text
-    print(text_response)
     return get_number_from_wolfram_response(text_response)
