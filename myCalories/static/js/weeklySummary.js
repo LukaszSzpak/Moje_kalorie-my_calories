9c4801c2b86731f4df885e15a5e324c8b1bd9b38
Łukasz Szpak <alukasz1@interia.pl>
2021-01-27 16:45:33 +0100

    Added pl/en table header and side column

--- myCalories/static/js/weeklySummary.js
+++ myCalories/static/js/weeklySummary.js
@@ -16,6 +16,33 @@ function parseWeeklyData(newWeek, lang) {
 
             document.getElementById('next_week').innerHTML = next_week[0] + ' - ' + next_week[0];
             document.getElementById('next_week').onclick = function () {parseWeeklyData(next_week[0], lang)};
+
+            weeklyNutrition(null, lang);
         }
     })
+}
+
+function weeklyNutrition(daysSummaries, lang) {
+    let caloriesRow = '';
+    let fatsRow = '';
+    let proteinsRow = '';
+    let carbohydratesRow = '';
+
+    if (lang === "pl") {
+        caloriesRow += '<td>Kalorie:</td>';
+        fatsRow += '<td>Tłuszcze:</td>';
+        proteinsRow += '<td>Białka:</td>';
+        carbohydratesRow += '<td>Węglowodany:</td>';
+    } else {
+        caloriesRow += '<td>Calories:</td>';
+        fatsRow += '<td>Fats:</td>';
+        proteinsRow += '<td>Proteins:</td>';
+        carbohydratesRow += '<td>Carbohydrates:</td>';
+    }
+
+    document.getElementById("weekly_calories").innerHTML = caloriesRow;
+    document.getElementById("weekly_fats").innerHTML = fatsRow;
+    document.getElementById("weekly_carbohydrates").innerHTML = carbohydratesRow;
+    document.getElementById("weekly_proteins").innerHTML = proteinsRow;
+
 }
